2b6cdb1d5234285f7ae2798130640510815164bd
Tarun Gunaseelan <tarun.gunaseelan@gmail.com>
2023-08-15 13:16:06 -0400

    new ui

--- pages/contact.js
+++ pages/contact.js
@@ -1,29 +1,47 @@
 
-import Link from 'next/link';
+import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Contact() {
     return (
-        <div>
-            <h1>Contact</h1>
-            <p>If you're interested in hiring me or just want to say hi, you can fill out the form below or reach out via LinkedIn, GitHub, or Twitter.</p>
-            <form>
-                <label>Name:</label>
-                <input type="text" required />
+        <div className="min-h-screen flex flex-col bg-gray-100">
+            <Head>
+                <title>Contact</title>
+                <meta name="description" content="Contact Me" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-                <label>Email:</label>
-                <input type="email" required />
+            {/* Header */}
+            <header className="bg-blue-600 text-white p-5">
+                <h1 className="text-xl font-bold">My Portfolio</h1>
+                <nav className="mt-4">
+                    <ul className="flex space-x-4">
+                        <li><Link href="/"><a className="hover:underline">Home</a></Link></li>
+                        <li><Link href="/education"><a className="hover:underline">Education</a></Link></li>
+                        <li><Link href="/experience"><a className="hover:underline">Experience</a></Link></li>
+                        <li><Link href="/skills"><a className="hover:underline">Skills</a></Link></li>
+                        <li><Link href="/projects"><a className="hover:underline">Projects</a></Link></li>
+                        <li><Link href="/contact"><a className="hover:underline">Contact</a></Link></li>
+                    </ul>
+                </nav>
+            </header>
 
-                <label>Message:</label>
-                <textarea required></textarea>
+            {/* Main Content */}
+            <main className="flex-grow p-5">
+                <h2 className="text-2xl mb-4">Get In Touch</h2>
+                <p className="mb-4">
+                    If you have any questions, project inquiries, or just want to say hi, feel free to drop me a message at <a href="mailto:example@email.com" className="text-blue-600 underline">example@email.com</a>.
+                </p>
+                <p className="mb-4">
+                    You can also find me on <a href="https://github.com/txrunn" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">GitHub</a>.
+                </p>
+            </main>
 
-                <button type="submit">Send</button>
-            </form>
-            <div>
-                <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
-                <a href="https://github.com/txrunn">GitHub</a>
-                <a href="https://twitter.com/yourusername">Twitter</a>
-            </div>
-            <Link href="/">Go Back</Link>
+            {/* Footer */}
+            <footer className="bg-blue-600 text-white p-5 text-center">
+                Copyright © 2023
+            </footer>
         </div>
     )
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,52 @@
 
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>My Portfolio</title>
-        <meta name="description" content="My Portfolio Website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="min-h-screen flex flex-col bg-gray-100">
+            <Head>
+                <title>My Portfolio</title>
+                <meta name="description" content="My Portfolio Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <header className={styles.header}>
-        <h1>Welcome to My Portfolio</h1>
-        <nav>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}><Link href="/about">About</Link></li>
-            <li className={styles.navItem}><Link href="/projects">Projects</Link></li>
-            <li className={styles.navItem}><Link href="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-      </header>
+            {/* Header */}
+            <header className="bg-blue-600 text-white p-5">
+                <h1 className="text-xl font-bold">My Portfolio</h1>
+                <nav className="mt-4">
+                    <ul className="flex space-x-4">
+                        <li><Link href="/"><a className="hover:underline">Home</a></Link></li>
+                        <li><Link href="/education"><a className="hover:underline">Education</a></Link></li>
+                        <li><Link href="/experience"><a className="hover:underline">Experience</a></Link></li>
+                        <li><Link href="/skills"><a className="hover:underline">Skills</a></Link></li>
+                        <li><Link href="/projects"><a className="hover:underline">Projects</a></Link></li>
+                        <li><Link href="/contact"><a className="hover:underline">Contact</a></Link></li>
+                    </ul>
+                </nav>
+            </header>
 
-      <main className={styles.main}>
-        <h2>Introduction</h2>
-        <p>
-          Hello! I'm a Cloud/SRE/DevOps engineer with extensive experience in managing and deploying scalable applications. This website showcases my projects and demonstrates my skills and understanding in this domain.
-        </p>
-        <h2>Skills</h2>
-        <ul>
-          <li>Cloud Infrastructure (AWS, GCP, Azure)</li>
-          <li>Container Orchestration (Kubernetes, Docker Swarm)</li>
-          <li>Infrastructure as Code (Terraform, CloudFormation)</li>
-          <li>CI/CD (Jenkins, Travis CI, GitHub Actions)</li>
-          <li>Configuration Management (Ansible, Chef, Puppet)</li>
-        </ul>
-      </main>
+            {/* Main Content */}
+            <main className="flex-grow p-5">
+                <h2 className="text-2xl mb-4">Introduction</h2>
+                <p className="mb-4">
+                    Hello! I'm a Cloud/SRE/DevOps engineer with extensive experience in managing and deploying scalable applications. This website showcases my projects and demonstrates my skills and understanding in this domain.
+                </p>
+                <h2 className="text-2xl mb-4">Skills</h2>
+                <ul className="list-disc list-inside pl-5">
+                    <li>Cloud Infrastructure (AWS, GCP, Azure)</li>
+                    <li>Container Orchestration (Kubernetes, Docker Swarm)</li>
+                    <li>Infrastructure as Code (Terraform, CloudFormation)</li>
+                    <li>CI/CD (Jenkins, Travis CI, GitHub Actions)</li>
+                    <li>Configuration Management (Ansible, Chef, Puppet)</li>
+                </ul>
+            </main>
 
-      <footer className={styles.footer}>
-        Copyright © 2023
-      </footer>
-    </div>
-  )
+            {/* Footer */}
+            <footer className="bg-blue-600 text-white p-5 text-center">
+                Copyright © 2023
+            </footer>
+        </div>
+    )
 }
+

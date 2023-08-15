e090efdc9e7440b0660088e31dc64e64913268d9
Tarun Gunaseelan <tarun.gunaseelan@gmail.com>
2023-08-15 12:13:04 -0400

    Added tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
